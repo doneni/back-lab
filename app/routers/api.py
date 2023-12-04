1ea81da545e6780d17bb4f64bcda173bfb122bbd
doneni <xxerrenn@icloud.com>
2023-12-04 12:56:33 +0900

    add dummy routes challenge.py + story.py

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
 
-from . import login, users
+from . import login, users, challenge, story
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-
+api_router.include_router(challenge.router, prefix="/challenge", tags=["challenge"])
+api_router.include_router(story.router, prefix="/story", tags=["story"])
 
 @api_router.get("/")
 async def root():
