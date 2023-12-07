7ea8889df96b53ad60ef0eb4fd896748976fd048
doneni <xxerrenn@icloud.com>
2023-12-07 19:04:17 +0900

    [db] *todo* internal server error

--- app/routers/challenge.py
+++ app/routers/challenge.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from typing import List
+
+from ..models.challenges import Challenge
+from ..schemas.challenges import ChallengeCreate
 
 router = APIRouter()
 
-@router.get("/")
-async def get_challenge():
-    return {"message": "Get Challenge Home !"}
+# @router.get("/")
+# async def root():
+#     return {"message": "Get Challenge Home !"}
+
+@router.get("/", response_model=List[Challenge])
+async def get_challenges():
+    try:
+        challenges = await Challenge.all()
+        return challenges
+    except Exception as e:
+        return {"error": f"An error occurred: {e}"}
