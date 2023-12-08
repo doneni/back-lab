339770c5040981eb4a49d0d3b97265e7e1afd67f
doneni <xxerrenn@icloud.com>
2023-12-08 18:07:03 +0900

    [unstable] append solved record submitting correct flag

--- app/routers/challenge.py
+++ app/routers/challenge.py
@@ -1,8 +1,9 @@
-# routers/challenges.py
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from app.models.challenges import Challenge
 from app.schemas.challenges import ChallengeBase
+from app.models.users import User
+from app.auth.auth import get_current_user
 
 router = APIRouter()
 
@@ -16,9 +17,15 @@ async def get_all_challenges():
     return {"challenges": challenges}
 
 @router.post("/check-flag")
-async def check_flag(title: str, user_flag: str):
+async def check_flag(
+        title: str,
+        user_flag: str,
+        current_user: User = Depends(get_current_user),
+):
     challenge = await Challenge.find_one({"title": title, "flag": user_flag})
     if challenge:
+        current_user.solved.append(str(challenge.uuid))
+        await current_user.save()
         return {"message": "correct flag"}
     else:
         return {"message": "incorrect flag"}
