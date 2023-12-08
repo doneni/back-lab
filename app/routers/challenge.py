8100ba8d8c32ad4f01954675b59fa10444b01744
doneni <xxerrenn@icloud.com>
2023-12-08 13:08:39 +0900

    routing get-challenges + check-flag

--- app/routers/challenge.py
+++ app/routers/challenge.py
@@ -1,19 +1,23 @@
-from fastapi import APIRouter, HTTPException
-from typing import List
+# routers/challenges.py
 
-from ..models.challenges import Challenge
-from ..schemas.challenges import ChallengeCreate
+from fastapi import APIRouter
+from app.models.challenges import Challenge
 
 router = APIRouter()
 
-# @router.get("/")
-# async def root():
-#     return {"message": "Get Challenge Home !"}
+@router.get("/")
+async def root_challenge():
+    return {"message": "Welcome to challenge home !"}
 
-@router.get("/", response_model=List[Challenge])
-async def get_challenges():
-    try:
-        challenges = await Challenge.all()
-        return challenges
-    except Exception as e:
-        return {"error": f"An error occurred: {e}"}
+@router.get("/get-challenges")
+async def get_all_challenges():
+    challenges = await Challenge.all().to_list()
+    return {"challenges": challenges}
+
+@router.post("/check-flag")
+async def check_flag(title: str, user_flag: str):
+    challenge = await Challenge.find_one({"title": title, "flag": user_flag})
+    if challenge:
+        return {"message": "correct flag"}
+    else:
+        return {"message": "incorrect flag"}
